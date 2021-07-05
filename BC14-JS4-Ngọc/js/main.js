e2d5ea19930a0a2a6f0f0d489daab387fc5d86cb
tranxuanngoc123 <85426749+tranxuanngoc123@users.noreply.github.com>
2021-07-05 23:15:51 +0700

    Add files via upload

---
+++ BC14-JS4-Ngọc/js/main.js
@@ -0,0 +1,57 @@
+//bài tập 2
+console.log("Hi cả nhà, ai là người đang sử dụng máy?");
+var a= "E";
+if (a === "M")
+{
+    console.log("Chào mẹ");
+}
+else if (a ==="B")
+{
+    console.log("Chào ba");
+}
+if (a ==="A")
+{
+    console.log("Chào em trai ");
+}
+if (a ==="E")
+{
+    console.log("Chào chị gái ");
+}
+
+//bài tập 4
+var c1, c2, c3;
+c1 = 3;
+c2 = 6;
+c3 = 5;
+if (c1 === c2 || c1 === c3 || c2 === c3)
+{
+    console.log("tác giác cân");
+}else if (c1 === c2 === c3)
+{
+    console.log("tam giác đều");
+
+}
+else (c1 ** 2 + c2 ** 2 === c3 ** 2 ||
+    c1 ** 2 + c3 ** 2 === c2 ** 2 ||
+    c2 ** 2 + c3 ** 2 === c1 **2
+    )
+    {
+        console.log("tam giác vuông")
+    }
+
+// bài tập 3
+var s1, s2, s3;
+s1 = 20;
+s2 = 25;
+s3 = 45;
+
+if (s2 % 2 == 0 ) {
+    console.log("đây là số chẵn");
+} else {
+    console.log("đây là số lẻ");
+}
+
+// bài tập 1
+
+
+
